e3a1978c59ca171438307c8e3108d6b27ad7bd10
AzmatKhan12 <rehan.dgms@gmail.com>
2022-09-17 19:34:42 +0530

    Store details on screen

--- main.js
+++ main.js
@@ -31,10 +31,12 @@ function onSubmit(e) {
 
 
 
+const item = document.querySelector('.items')
+item.remove();
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
-
+const userList = document.querySelector('#users');
 myForm.addEventListener('submit', onSubmit);
 function onSubmit(e) {
     e.preventDefault();
@@ -56,5 +58,45 @@ function onSubmit(e) {
     let user = JSON.parse(localStorage.getItem('Details'));
 
     console.log(user);
+    showOnScreen(userDetails);
+
+}
+
+// Object.keys(localStorage).forEach((key) => {
+//     stringifiedDetailsOfPeople = localStorage.getItem(key);
+//     detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);
+//     addNewLineElement(detailsOfPeople);
+
+// });
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    const localStorageObj = localStorage;
+    const localstoragekeys = Object.keys(localStorageObj)
+
+    for (var i = 0; i < localstoragekeys.length; i++) {
+        const key = localstoragekeys[i]
+        const userDetailsString = localStorageObj[key];
+        const userDetailsObj = JSON.parse(userDetailsString);
+        showOnScreen(userDetailsObj)
+    }
+})
+
+function showOnScreen(user) {
+
+    document.getElementById('email').value = '';
+    document.getElementById('name').value = '';
+
+
+    // if (localStorage.getItem(user.email) !== null) {
+    //     remove(user.email)
+    // }
+
+    const parentNode = userList;
+    const childHTML = `<li>${user.name} : ${user.email}</li>`;
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+
+
+
 
 }
